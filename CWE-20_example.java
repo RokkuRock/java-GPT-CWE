f5f68461e37701df093220c44bc6bb079fcf3e4a
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:21:34 +0800

    Update CWE-20_example.java

--- CWE-20_example.java
+++ CWE-20_example.java
@@ -1,12 +1,12 @@
-// File: ImproperInput.java
-import java.util.Scanner;
+// File: Cwe20_ArrayIndex.java
+import java.util.*;
 
-public class ImproperInput {
+public class Cwe20_ArrayIndex {
     public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5};
         Scanner sc = new Scanner(System.in);
-        System.out.print("Array size: ");
-        int size = sc.nextInt(); // CWE-20
-        int[] arr = new int[size]; // 允許 size 為負或過大
-        System.out.println("Created array with length " + arr.length);
+        System.out.print("index to read [0-4]: ");
+        int idx = sc.nextInt(); // CWE-20
+        System.out.println("Value: " + arr[idx]); // 可能越界
     }
 }
