9bc6d4e9b6fd97a577afe45414d8104b46cf49ec
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:22:39 +0800

    Update CWE-74_XSLT_example.java

--- CWE-74_XSLT_example.java
+++ CWE-74_XSLT_example.java
@@ -1,13 +1,15 @@
-// File: XsltInjection.java
+// File: Cwe74_Xslt.java
 import javax.xml.transform.*;
 import javax.xml.transform.stream.*;
 import java.io.*;
 
-public class XsltInjection {
+public class Cwe74_Xslt {
     public static void main(String[] args) throws Exception {
-        TransformerFactory tf = TransformerFactory.newInstance();
-        // CWE-74: 使用外部用戶上傳 wslt.xsl
-        Transformer t = tf.newTransformer(new StreamSource(new File("wslt.xsl")));
-        t.transform(new StreamSource(new File("input.xml")), new StreamResult(System.out));
+        System.out.print("XSLT path: ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String xslt = br.readLine();
+        Transformer t = TransformerFactory.newInstance()
+            .newTransformer(new StreamSource(new File(xslt))); // CWE-74
+        t.transform(new StreamSource(new File("in.xml")), new StreamResult(System.out));
     }
 }
