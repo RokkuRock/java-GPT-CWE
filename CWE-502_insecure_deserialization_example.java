5f0de22ee3b34ecc2daeac5793f73edf3506fbe6
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:07:42 +0800

    Create CWE-502_insecure_deserialization_example.java

---
+++ CWE-502_insecure_deserialization_example.java
@@ -0,0 +1,14 @@
+// File: UnsafeDeserialization.java
+import java.io.*;
+import java.net.*;
+public class UnsafeDeserialization {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(9000);
+        Socket s = ss.accept();
+        ObjectInputStream ois = new ObjectInputStream(s.getInputStream());
+        Object o = ois.readObject(); // CWE-502
+        System.out.println("Got: " + o);
+        ois.close();
+        ss.close();
+    }
+}
