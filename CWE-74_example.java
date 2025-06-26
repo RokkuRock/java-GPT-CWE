af16161b162b4ca2a4e36c1ee3f90a21e108d266
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 09:22:24 +0800

    Update CWE-74_example.java

--- CWE-74_example.java
+++ CWE-74_example.java
@@ -1,13 +1,12 @@
-// File: JndiInjection.java
+// File: Cwe74_JNDI.java
 import javax.naming.*;
 import java.util.*;
 
-public class JndiInjection {
+public class Cwe74_JNDI {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter JNDI URL: "); // e.g. ldap://evil
-        String url = sc.nextLine();
-        new InitialContext().lookup(url); // CWE-74: 任意 JNDI lookup
+        System.out.print("Enter JNDI lookup URL: ");
+        String url = new Scanner(System.in).nextLine();
+        new InitialContext().lookup(url); // CWE-74
         System.out.println("Lookup done");
     }
 }
