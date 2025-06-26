97afca3537e5306e790f51f4df110d101dd0bfd0
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-26 21:29:15 +0800

    Update CWE-327_MD5_example.java

--- CWE-327_MD5_example.java
+++ CWE-327_MD5_example.java
@@ -1,9 +1,8 @@
-// File: WeakHash.java
 import java.security.*;
 
 public class WeakHash {
     public static void main(String[] args) throws Exception {
-        MessageDigest md = MessageDigest.getInstance("MD5"); // CWE-327
+        MessageDigest md = MessageDigest.getInstance("MD5"); 
         byte[] d = md.digest("sensitive-data".getBytes());
         System.out.println(javax.xml.bind.DatatypeConverter.printHexBinary(d));
     }
