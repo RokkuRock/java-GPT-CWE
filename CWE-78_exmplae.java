0785d47c185e20a2dc876ac1734f59be8e370235
Rock Li <76486033+RokkuRock@users.noreply.github.com>
2025-06-27 14:53:47 +0800

    Update CWE-78_exmplae.java

--- CWE-78_exmplae.java
+++ CWE-78_exmplae.java
@@ -1,13 +1,18 @@
 import java.io.*;
+import java.util.regex.*;
 
 public class CmdExec {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.print("Enter directory to list: ");
         String dir = br.readLine();
-        String cmd = "ls " + dir; 
-        Process p = Runtime.getRuntime().exec(cmd);
-        new BufferedReader(new InputStreamReader(p.getInputStream()))
-            .lines().forEach(System.out::println);
+        if (Pattern.matches("[a-zA-Z0-9_/]*", dir)) {
+            String cmd = "ls " + dir; 
+            Process p = Runtime.getRuntime().exec(cmd);
+            new BufferedReader(new InputStreamReader(p.getInputStream()))
+               .lines().forEach(System.out::println);
+        } else {
+            System.out.println("Invalid input");
+        }
     }
 }
